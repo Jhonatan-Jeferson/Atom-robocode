f0c442663d95d15cb5587ef5a657e9b904a4eba0
Jhonatan-Jeferson <jhonatanjefersonoliveira@gmail.com>
2024-09-12 21:04:17 -0300

    Código base

---
+++ Atom.java
@@ -0,0 +1,63 @@
+package atom;
+import robocode.*;
+import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Atom - a robot by (your name here)
+ */
+public class Atom extends Robot
+{
+	/**
+	 * run: Atom's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		setColors(Color.orange.brighter(),Color.black.darker(),Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+		/*	ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		*/
+			ahead(50);
+			turnRight(30);
+			ahead(50);
+			turnRight(30);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		ahead(50);
+		turnLeft(50);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+		turnRight(60);
+	}	
+}
